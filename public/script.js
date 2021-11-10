5b38d5eeccc8a63fd963d8ff6a37fdec8720a72d
Stu Dev <srinikesh.lidolearning.12@gmail.com>
2021-11-10 09:52:04 +0530

    firebase problem resolved

--- public/script.js
+++ public/script.js
@@ -1,4 +1,30 @@
-async function myfunc(){
+if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('./weathernowSw.js');
+}
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAn9EbHkivV2wmCuTKs7_879pvfHNcUzjc",
+  authDomain: "weather-now-839ac.firebaseapp.com",
+  projectId: "weather-now-839ac",
+  storageBucket: "weather-now-839ac.appspot.com",
+  messagingSenderId: "97809170483",
+  appId: "1:97809170483:web:3564ef8133056b01e12346",
+  measurementId: "G-CGY1J3YJ35"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+async function myfunc() {
   var input = document.getElementById("city").value;
   var url = "https://api.openweathermap.org/data/2.5/weather?q=" + input + "&appid=49fd6f61e2b89421e0c3b527bc26efd6";
   var response = await fetch(url);
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+const path = require('path');
+module.exports = {
+    entry: './public/script.js',
+    output: {
+        filename: 'index.js',
+        path:path.resolve(__dirname, 'public')
+    }
+}
