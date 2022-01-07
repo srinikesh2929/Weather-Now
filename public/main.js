fb04db93746d7a4aa64028e122356f5dab8c07bc
Stu Dev <srinikesh.lidolearning.12@gmail.com>
2022-01-07 13:03:52 +0530

    asking for the temperature unit

--- public/main.js
+++ public/main.js
@@ -1,23 +1,41 @@
 var place = document.getElementById("place");
 var temp = document.getElementById("temp");
 var capitalizeString=(str)=>str[0].toUpperCase()+str.slice(1).toLowerCase()
+const tempOpt=prompt("Do you want temperature in Celsius or Farenheit?(type C or F)");
 async function myfunc() {
-var input = capitalizeString(document.getElementById("city").value);
-var url = "https://api.openweathermap.org/data/2.5/weather?q=" + input + "&appid=49fd6f61e2b89421e0c3b527bc26efd6";
+  var input = capitalizeString(document.getElementById("city").value);
+  var url = "https://api.openweathermap.org/data/2.5/weather?q=" + input + "&appid=49fd6f61e2b89421e0c3b527bc26efd6";
     try {
         var response = await fetch(url);
         var responseJSON = await response.json();
         var placeV = input + " , " + responseJSON.sys['country'];
         var weather = responseJSON.weather[0]['main'];
         var tempInC = (responseJSON.main['temp'] - 273.15).toFixed(2) + " C";
+        var tempInF = ((responseJSON.main['temp']-273.15) * 9 / 5 +32).toFixed(2) + " F";
       place.innerHTML = placeV;
-      temp.innerHTML = tempInC + " , " + weather;
+      switch (tempOpt) {
+        case "C":
+          temp.innerHTML = tempInC + " , " + weather;
+          break;
+        case "c":
+          temp.innerHTML = tempInC + " , " + weather;
+          break;  
+        case "F":
+          temp.innerHTML = tempInF + " , " + weather;
+          break;
+        case "f":
+          temp.innerHTML = tempInF + " , " + weather;
+          break;
+      }
     } catch(err) { 
-        alert('Please type a city name or Check your internet connection');
+      alert('Please type a city name or Check your internet connection');
     }
   var icon = await responseJSON.weather[0]['icon'];
   var iconImage = await "http://openweathermap.org/img/wn/"+ icon + "@4x.png";
   var image = document.getElementById("icon");
   image.src = iconImage;
   document.getElementById("output").style.visibility = "visible";
+  console.log(responseJSON);
+  console.log(responseJSON.main['temp']);
+  console.log(tempInF);
 }
