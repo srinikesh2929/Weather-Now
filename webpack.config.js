38866c45a90f5387ca3842c960bd1dd87c6cc0b6
Srinikesh Srinivasan <srinikesh.lidolearning.12@gmail.com>
2024-05-09 22:08:35 +0530

    removing firebase

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-module.exports = {
-    entry: './public/script.js',
-    output: {
-        filename: 'index.js',
-        path:path.resolve(__dirname, './public')
-    },
-    mode:'development'
-}
